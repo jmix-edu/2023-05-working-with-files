451fd95f19d779547b62ecf17c671e4964031fe3
Gleb Gorelov <g.gorelov@haulmont.com>
2023-05-03 18:36:28 +0400

    Part 1

--- src/main/java/com/company/tm/screen/task/TaskEdit.java
+++ src/main/java/com/company/tm/screen/task/TaskEdit.java
@@ -4,6 +4,8 @@ import com.company.tm.entity.Project;
 import com.company.tm.entity.Task;
 import com.company.tm.entity.User;
 import io.jmix.core.usersubstitution.CurrentUserSubstitution;
+import io.jmix.ui.component.BrowserFrame;
+import io.jmix.ui.component.FileStorageResource;
 import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +18,18 @@ public class TaskEdit extends StandardEditor<Task> {
     @Autowired
     private CurrentUserSubstitution currentUserSubstitution;
 
+    @Autowired
+    private BrowserFrame attachmentBrowserFrame;
+
     @Subscribe
     public void onInitEntity(InitEntityEvent<Task> event) {
         User user = (User) currentUserSubstitution.getEffectiveUser();
         event.getEntity().setAssignee(user);
+    }
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        refreshAttachmentPreview();
     }
 
     @Subscribe(id = "taskDc", target = Target.DATA_CONTAINER)
@@ -29,6 +39,19 @@ public class TaskEdit extends StandardEditor<Task> {
             if (newProject != null) {
                 event.getItem().setPriority(newProject.getDefaultTaskPriority());
             }
+        }
+
+        if ("attachment".equals(event.getProperty())) {
+            refreshAttachmentPreview();
+        }
+    }
+
+    private void refreshAttachmentPreview() {
+        Task task = getEditedEntity();
+        if (task.getAttachment() != null) {
+            attachmentBrowserFrame.setSource(FileStorageResource.class)
+                    .setFileReference(task.getAttachment())
+                    .setMimeType(task.getAttachment().getContentType());
         }
     }
 }
